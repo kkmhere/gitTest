44608bf884cfacd6ff9c1255cac6d2917997f2d4
kvk123 <kv58265@gmail.com>
2019-01-13 14:39:09 +0530

    Splash Added

--- app/src/main/java/com/kkm/www/gittest/MainActivity.java
+++ app/src/main/java/com/kkm/www/gittest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.kkm.www.gittest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,6 +10,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread t =new Thread(){
+
+            @Override
+            public void run() {
+                super.run();
+
+                try {
+                    sleep(3000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                finally {
+                    startActivity(new Intent(getApplicationContext(),Main2Activity.class));
+                    MainActivity.this.finish();
+                }
+            }
+        };t.start();
     }
 }
-//Supercode goes here....
