b37d39bb4f6a941c3268508e0535506d39449ba6
kvk123 <kv58265@gmail.com>
2019-01-15 11:07:31 +0530

    Connecting Activities

--- app/src/main/java/com/kkm/www/gittest/Main2Activity.java
+++ app/src/main/java/com/kkm/www/gittest/Main2Activity.java
@@ -1,13 +1,31 @@
 package com.kkm.www.gittest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        button = findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity3();
+            }
+        });
+    }
+
+    public void openActivity3()
+    {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
     }
 }
