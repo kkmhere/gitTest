cfb068527e4ce53b8b9465b665fb735cf6704d36
kvk123 <kv58265@gmail.com>
2019-01-15 17:13:49 +0530

    Locations

--- app/src/main/java/com/kkm/www/gittest/Main3Activity.java
+++ app/src/main/java/com/kkm/www/gittest/Main3Activity.java
@@ -1,13 +1,30 @@
 package com.kkm.www.gittest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main3Activity extends AppCompatActivity {
+    private Button click;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        click = findViewById(R.id.button3);
+        click.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivity4();
+            }
+        });
+    }
+
+    public void openActivity4() {
+        Intent intent = new Intent(this, Main4Activity.class);
+        startActivity(intent);
     }
 }
