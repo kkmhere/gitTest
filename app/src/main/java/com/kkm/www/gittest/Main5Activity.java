3210a3171afc226b35811b1713a03eab00ecb98b
kvk123 <kv58265@gmail.com>
2019-01-25 02:13:33 +0530

    Edited 3

--- app/src/main/java/com/kkm/www/gittest/Main5Activity.java
+++ app/src/main/java/com/kkm/www/gittest/Main5Activity.java
@@ -2,6 +2,8 @@ package com.kkm.www.gittest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class Main5Activity extends AppCompatActivity {
 
@@ -9,5 +11,12 @@ public class Main5Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.spinner2);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<>(Main5Activity.this,
+                android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.locations));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
     }
 }
