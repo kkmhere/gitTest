afc0a95384551b8f96ca3341ffc6ac0023c1da74
kvk123 <kv58265@gmail.com>
2019-02-13 21:05:21 +0530

    Activity 5 update

--- app/src/main/java/com/kkm/www/gittest/Main5Activity.java
+++ app/src/main/java/com/kkm/www/gittest/Main5Activity.java
@@ -19,10 +19,7 @@ public class Main5Activity extends AppCompatActivity {
         textView.setText("KERELA DETAILS HERE");
     }
 
-    public void karnataka_detials(View view)
-    {
-        textView.setText("1. Long lived palm living generally to an age of about 80 to 90 years.\\n2. Palms thrive well under different soil conditions varying from littoral sands to red loams and laterites.\\n3. Palms grow well up to an altitude of 3,000 ft. above the sea level. It is fairly resistant to diseases and pests.\\n4. The tree attains a height of about 15m to 18m or more.\\n5. It begins to bear in about 8 to 10 years after planting.\\n6. The nut is medium to big in size varying in shape from spheroid to linear-oblong and with colors varying from green, yellow and orange to shades of brown.\\n7. About 6,000 nuts yield a ton of copra.");
-    }
+    public void karnataka_details(View view) { textView.setText("KARNATAKA DETAILS"); }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
