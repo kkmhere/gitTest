b4343d6a721485af4b638d07016918800cb103f8
kvk123 <kv58265@gmail.com>
2019-02-26 15:04:59 +0530

    Tall Varieties

--- app/src/main/java/com/kkm/www/gittest/Main5Activity.java
+++ app/src/main/java/com/kkm/www/gittest/Main5Activity.java
@@ -1,32 +1,58 @@
 package com.kkm.www.gittest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.Button;
 import android.widget.TextView;
 
 public class Main5Activity extends AppCompatActivity {
-    TextView textView;
-    public void tamil_details(View view)
-    {
-        textView.setText("TAMIL NADU DETAILS HERE");
-    }
-
-    public void kerela_details(View view)
-    {
-        textView.setText("KERELA DETAILS HERE");
-    }
-
-    public void karnataka_details(View view) { textView.setText("KARNATAKA DETAILS"); }
+    private Button c1,c2,c3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main5);
-        textView=(TextView)findViewById(R.id.cocoDetails);
 
+        c1 = findViewById(R.id.kar);
+        c1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivitykt();
+            }
+        });
 
+        c2 = findViewById(R.id.tamil);
+        c2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivitytt();
+            }
+        });
+
+        c3 = findViewById(R.id.kerela);
+        c3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                openActivityket();
+            }
+        });
+
+    }
+
+    public void openActivitykt() {
+        Intent intent = new Intent(this, Karnataka_tall.class);
+        startActivity(intent);
+    }
+
+    public void openActivitytt() {
+        Intent intent = new Intent(this, TamilNadu_tall.class);
+        startActivity(intent);
+    }
+
+    public void openActivityket() {
+        Intent intent = new Intent(this, Main5Activity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/kkm/www/gittest/TamilNadu_tall.java
@@ -0,0 +1,13 @@
+package com.kkm.www.gittest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TamilNadu_tall extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tamil_nadu_tall);
+    }
+}
